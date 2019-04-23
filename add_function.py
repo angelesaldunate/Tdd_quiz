baa2e1a61ee386cef14d015fdeee9e5f2b06a213
angelesaldunate <31292145+angelesaldunate@users.noreply.github.com>
2019-04-23 19:55:38 -0400

    All test passing and functionality done

--- add_function.py
+++ add_function.py
@@ -2,8 +2,8 @@
 
 
 def add_function(numbers, delimiter):
-    if delimiter!= "":
-        list_numbers = list(map(int, numbers.split(delimiter)))
-    else:
-        list_numbers = list(map(int, numbers.split(',')))
+    if delimiter == "":
+        delimiter = ","
+    list_numbers = list(map(int, numbers.split(delimiter)))
+    list_numbers = [item for item in list_numbers if item >= 0]
     return sum(list_numbers)
