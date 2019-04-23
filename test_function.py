ade577010a67ee5c859434945cb5de0935727bf1
angelesaldunate <31292145+angelesaldunate@users.noreply.github.com>
2019-04-23 19:50:12 -0400

    fixed old tests to new requirement of delimiter

--- test_function.py
+++ test_function.py
@@ -5,19 +5,20 @@ from add_function import add_function
 class TestsAdd(unittest.TestCase):
     def setUp(self):
         self.numbers = "1,2,3,1"
+        self.numbers1 = "1;2;3;1"
 
     def test_add_receiving_string(self):
-        expected_result = add_function(self.numbers)
+        expected_result = add_function(self.numbers, "")
         self.failureException(False, expected_result )
 
     def test_add_return_list(self):
-        expected_result = add_function(self.numbers)
+        expected_result = add_function(self.numbers, "")
         self.assertIsInstance(expected_result, int)
 
     def test_sum_of_numbers(self):
-        expected_result = add_function(self.numbers)
+        expected_result = add_function(self.numbers, "")
         self.assertEqual(7, expected_result)
 
     def test_specify_string(self):
-        expected_result = add_function(self.numbers, ";")
+        expected_result = add_function(self.numbers1, ";")
         self.assertEqual(7, expected_result)
