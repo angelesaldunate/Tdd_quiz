9fd9359f03f867effddecf43de71ea78490f2a6f
angelesaldunate <31292145+angelesaldunate@users.noreply.github.com>
2019-04-23 19:52:17 -0400

    Added test to dissmis negative numbers failing

--- test_function.py
+++ test_function.py
@@ -6,6 +6,7 @@ class TestsAdd(unittest.TestCase):
     def setUp(self):
         self.numbers = "1,2,3,1"
         self.numbers1 = "1;2;3;1"
+        self.numbers2 = "1,2,3,-1"
 
     def test_add_receiving_string(self):
         expected_result = add_function(self.numbers, "")
@@ -22,3 +23,7 @@ class TestsAdd(unittest.TestCase):
     def test_specify_string(self):
         expected_result = add_function(self.numbers1, ";")
         self.assertEqual(7, expected_result)
+
+    def test_dismiss_negatives(self):
+        expected_result = add_function(self.numbers2, "")
+        self.assertEqual(6, expected_result)
