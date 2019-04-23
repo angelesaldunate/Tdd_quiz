a550a75b69844932a25196265387099cb6395a85
angelesaldunate <31292145+angelesaldunate@users.noreply.github.com>
2019-04-23 19:48:21 -0400

    Failing old tests

--- add_function.py
+++ add_function.py
@@ -1,4 +1,9 @@
 
-def add_function(numbers):
-    list_numbers = list(map(int, numbers.split(',')))
+
+
+def add_function(numbers, delimiter):
+    if delimiter!= "":
+        list_numbers = list(map(int, numbers.split(delimiter)))
+    else:
+        list_numbers = list(map(int, numbers.split(',')))
     return sum(list_numbers)
